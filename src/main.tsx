a88af0b6687d967552f0deb16b36fe6956de87bb
vwh <vwhe@proton.me>
2024-08-28 15:52:00 +0300

    refactor: remove theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import ThemeProvider from "@/components/theme/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
